957ea6db31ac29a81831d92ca46b73e3b791d6ea
avodhel <uyelik-eposta@hotmail.com>
2019-10-21 13:45:31 +0300

    explosion animation

--- Colorize the Ufo/Assets/Scripts/Controls/ExplosionControl.cs
+++ Colorize the Ufo/Assets/Scripts/Controls/ExplosionControl.cs
@@ -2,40 +2,22 @@
 
 public class ExplosionControl : MonoBehaviour {
 
-    [SerializeField]
-    [Header("Explosion")]
-    private Sprite[] explosionEffectSprites;
-
     private SpriteRenderer spriteRenderer;
 
-    private int explosionTimer = 0;
-    private float explosionEffectTimer = 0f;
+    public Animator anim;
 
     private void Start ()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
-	
-	private void Update ()
-    {
-        explosionEffect();
-	}
 
-    private void explosionEffect()
+    private void makeVisible()
     {
-        if (explosionEffectSprites.Length != 0)
-        {
-            explosionEffectTimer += Time.deltaTime;
-            if (explosionEffectTimer > 0.2f)
-            {
-                if (explosionTimer < explosionEffectSprites.Length)
-                {
-                    spriteRenderer.sprite = explosionEffectSprites[explosionTimer];
-                    gameObject.transform.localScale = new Vector3(2f, 2f, gameObject.transform.localScale.z);
-                    explosionTimer++;
-                    explosionEffectTimer = 0f;
-                }
-            }
-        }
+        spriteRenderer.enabled = true;
+    }
+
+    private void makeInvisible()
+    {
+        spriteRenderer.enabled = false;
     }
 }
